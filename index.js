8c8f6f311725de863ae59546ebd011939083d337
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-06-22 23:25:30 +0600

    Updated index

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-ÿþ
+const express = require("express");
+const app = express();
+
+// Config
+const PORT = process.env.PORT || 3000;
+
+// Routes
+app.get('/', (req, res) => {
+    return res.send("The server is working fine...");
+});
+
+app.listen(PORT, () => {
+    console.log(
+        `Listening on port: ${PORT}\n\n
+        Supported Methods: GET, POST, PUT, DELETE
+        \n====== Routes ======\n
+        /api/bugs\n
+        /api/bug/:id\n
+        `
+    );
+});
